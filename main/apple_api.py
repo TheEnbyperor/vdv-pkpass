615f7083db5f32f3b3f4040884f71e6b9611c878
Q Misell <q@magicalcodewit.ch>
2024-10-11 14:37:34 +0200

    refactor auth checking for apple API

--- main/apple_api.py
+++ main/apple_api.py
@@ -11,6 +11,41 @@ from . import models, views
 
 logger = logging.Logger(__name__)
 
+
+def check_pass_auth(f):
+    def wrapper(request, *, pass_type_id, serial_number, **kwargs):
+        if "Authorization" not in request.headers:
+            return HttpResponse(status=401)
+
+        auth_header = request.headers["Authorization"]
+        if not auth_header.startswith("ApplePass "):
+            return HttpResponse(status=401)
+
+        auth_token = auth_header[10:]
+
+        if pass_type_id != settings.PKPASS_CONF["pass_type"]:
+            return HttpResponse(status=404)
+
+        ticket_obj: models.Ticket = models.Ticket.objects.get(id=serial_number)
+        if not ticket_obj:
+            return HttpResponse(status=404)
+
+        if ticket_obj.pkpass_authentication_token != auth_token:
+            return HttpResponse(status=401)
+
+        return f(request, ticket_obj=ticket_obj, **kwargs)
+
+    return wrapper
+
+
+def ticket_updated_date(_request, pass_type_id, serial_number):
+    if pass_type_id != settings.PKPASS_CONF["pass_type"]:
+        return
+    ticket_obj: models.Ticket = models.Ticket.objects.get(id=serial_number)
+    if not ticket_obj:
+        return
+    return ticket_obj.last_updated
+
 @csrf_exempt
 def pass_status(request, device_id, pass_type_id):
     device_obj = models.AppleDevice.objects.get(device_id=device_id)
@@ -32,35 +67,20 @@ def pass_status(request, device_id, pass_type_id):
         regs = regs.filter(ticket__last_updated__gt=last_updated)
 
     tickets = [reg.ticket for reg in regs]
-    new_last_updated = max(ticket.last_updated for ticket in tickets)
+    new_last_updated = max(
+        (ticket.last_updated for ticket in tickets),
+        default=datetime.datetime.now(pytz.utc).timestamp()
+    )
 
     return HttpResponse(status=200, content_type="application/json", content=json.dumps({
-        "lastUpdated": str(int(new_last_updated.timestamp())),
+        "lastUpdated": str(int(new_last_updated.astimezone(pytz.utc).timestamp()) + 1),
         "serialNumbers": [str(ticket.id) for ticket in tickets]
     }))
 
 
 @csrf_exempt
-def registration(request, device_id, pass_type_id, serial_number):
-    if "Authorization" not in request.headers:
-        return HttpResponse(status=401)
-
-    auth_header = request.headers["Authorization"]
-    if not auth_header.startswith("ApplePass "):
-        return HttpResponse(status=401)
-
-    auth_token = auth_header[10:]
-
-    if pass_type_id != settings.PKPASS_CONF["pass_type"]:
-        return HttpResponse(status=404)
-
-    ticket_obj: models.Ticket = models.Ticket.objects.get(id=serial_number)
-    if not ticket_obj:
-        return HttpResponse(status=404)
-
-    if ticket_obj.pkpass_authentication_token != auth_token:
-        return HttpResponse(status=401)
-
+@check_pass_auth
+def registration(request, device_id, ticket_obj):
     if request.method == "POST":
         if request.content_type != "application/json":
             return HttpResponse(status=415)
@@ -103,36 +123,10 @@ def registration(request, device_id, pass_type_id, serial_number):
         return HttpResponse(status=405)
 
 
-def ticket_updated_date(_request, pass_type_id, serial_number):
-    if pass_type_id != settings.PKPASS_CONF["pass_type"]:
-        return
-    ticket_obj: models.Ticket = models.Ticket.objects.get(id=serial_number)
-    if not ticket_obj:
-        return
-    return ticket_obj.last_updated
-
 @csrf_exempt
 @condition(last_modified_func=ticket_updated_date)
-def pass_document(request, pass_type_id, serial_number):
-    if "Authorization" not in request.headers:
-        return HttpResponse(status=401)
-
-    auth_header = request.headers["Authorization"]
-    if not auth_header.startswith("ApplePass "):
-        return HttpResponse(status=401)
-
-    auth_token = auth_header[10:]
-
-    if pass_type_id != settings.PKPASS_CONF["pass_type"]:
-        return HttpResponse(status=404)
-
-    ticket_obj: models.Ticket = models.Ticket.objects.get(id=serial_number)
-    if not ticket_obj:
-        return HttpResponse(status=404)
-
-    if ticket_obj.pkpass_authentication_token != auth_token:
-        return HttpResponse(status=403)
-
+@check_pass_auth
+def pass_document(_, ticket_obj):
     return views.make_pkpass(ticket_obj)
 
 
