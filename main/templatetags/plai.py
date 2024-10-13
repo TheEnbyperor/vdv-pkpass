fb71e4c1547f529c0d2145507a4c662e39a0f7b8
Q Misell <q@magicalcodewit.ch>
2024-10-13 02:06:25 +0200

    auto PLAI height/width

---
+++ main/templatetags/plai.py
@@ -0,0 +1,18 @@
+import datetime
+import pytz
+import typing
+import iso3166
+from django import template
+from .. import uic
+
+register = template.Library()
+
+
+@register.filter(name="plai_width")
+def plai_width(fields: typing.List) -> int:
+    return max([f.column + f.width for f in fields])
+
+
+@register.filter(name="plai_height")
+def plai_height(fields: typing.List) -> int:
+    return max([f.row + f.height for f in fields])
