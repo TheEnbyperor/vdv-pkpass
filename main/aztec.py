9ba812d06d5f185bc41b9f5875aaa2717cc84769
Q Misell <q@magicalcodewit.ch>
2024-10-22 17:54:54 +0200

    use bakoder server side

--- main/aztec.py
+++ main/aztec.py
@@ -1,170 +1,34 @@
-import subprocess
-import zxingcpp
-import enum
-import PIL.Image
-import io
-from django.conf import settings
+import json
+import numpy as np
+import cv2
 
 
 class AztecError(Exception):
     pass
 
 
-class Table(enum.Enum):
-    UPPER = 0
-    LOWER = 1
-    MIXED = 2
-    DIGIT = 3
-    PUNCT = 4
-    BINARY = 5
-
-UPPER_TABLE = [
-    'CTRL_PS', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'CTRL_LL', 'CTRL_ML', 'CTRL_DL', 'CTRL_BS'
-]
-
-LOWER_TABLE = [
-    'CTRL_PS', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'CTRL_US', 'CTRL_ML', 'CTRL_DL', 'CTRL_BS'
-]
-
-MIXED_TABLE = [
-    'CTRL_PS', ' ', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', '\b', '\t', '\n',
-    '\x0b', '\f', '\r', '\x1b', '\x1c', '\x1d', '\x1e', '\x1f', '@', '\\', '^', '_',
-    '`', '|', '~', '\x7f', 'CTRL_LL', 'CTRL_UL', 'CTRL_PL', 'CTRL_BS'
-]
-
-PUNCT_TABLE = [
-    '', '\r', '\r\n', '. ', ', ', ': ', '!', '"', '#', '$', '%', '&', '\'', '(', ')',
-    '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '[', ']', '{', '}', 'CTRL_UL'
-]
-
-DIGIT_TABLE = [
-    'CTRL_PS', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.', 'CTRL_UL', 'CTRL_US'
-]
-
-def decode_boofcv(data: bytes) -> bytes:
+def decode(img_data: bytes):
     try:
-        p = subprocess.Popen(
-            ["java", "-jar", settings.AZTEC_JAR_PATH],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-            universal_newlines=False, text=False
-        )
-    except OSError as e:
-        raise AztecError("Could not execute Java binary") from e
+        from Barkoder import BarkoderSDK
+    except ImportError:
+        raise AztecError("Barkoder SDK not available")
 
-    stdout, _ = p.communicate(data)
-    if p.returncode == 255:
-        raise AztecError("Invalid image data")
-    elif p.returncode == 254:
-        raise AztecError("Failed to decode Aztec")
-    elif p.returncode != 0:
-        raise AztecError(f"Java process exited with code {p.returncode}")
-    elif len(stdout) == 0:
-        raise AztecError("No barcode was found in the image")
+    decoders = [
+        BarkoderSDK.constants["Decoders"]["Aztec"],
+        BarkoderSDK.constants["Decoders"]["AztecCompact"],
+    ]
+    BarkoderSDK.setEnabledDecoders(decoders, len(decoders))
+    BarkoderSDK.setDecodingSpeed(BarkoderSDK.constants["DecodingSpeed"]["Normal"])
 
-    out_lines = stdout.splitlines()
-    num_bits = int(out_lines[0], 10)
-    bits_str = out_lines[1]
-    bits = []
-    for bit in bits_str:
-        if bit == 0x31:
-            bits.append(True)
-        elif bit == 0x30:
-            bits.append(False)
+    img = cv2.imdecode(np.asarray(bytearray(img_data), dtype="uint8"), cv2.IMREAD_GRAYSCALE)
+    if not img:
+        raise AztecError("Unable to read image")
 
-    return get_encoded_data_from_bits(bits, num_bits)
+    height, width = img.shape[:2]
+    result_json = BarkoderSDK.decodeImage(img, width, height)
+    result = json.loads(result_json)
 
-def decode_zxing(data: bytes) -> bytes:
-    try:
-        img = PIL.Image.open(io.BytesIO(data))
-    except PIL.UnidentifiedImageError as e:
-        raise AztecError("Invalid image data") from e
-    code = zxingcpp.read_barcode(img, zxingcpp.Aztec)
-    if not code:
-        raise AztecError("Failed to decode Aztec")
-    return code.bytes
-
-def decode(data: bytes) -> bytes:
-    try:
-        return decode_boofcv(data)
-    except AztecError:
-        return decode_zxing(data)
-
-
-def get_encoded_data_from_bits(corrected_bits, end_index):
-    latch_table = Table.UPPER
-    shift_table = Table.UPPER
-    output = bytearray()
-    index = 0
-
-    while index < end_index:
-        if shift_table == Table.BINARY:
-            if end_index - index < 5:
-                break
-            length = read_code(corrected_bits, index, 5)
-            index += 5
-            if length == 0:
-                if end_index - index < 11:
-                    break
-                length = read_code(corrected_bits, index, 11) + 31
-                index += 11
-            for _ in range(length):
-                if end_index - index < 8:
-                    index = end_index
-                    break
-                code = read_code(corrected_bits, index, 8)
-                output.append(code)
-                index += 8
-            shift_table = latch_table
-        else:
-            size = 4 if shift_table == Table.DIGIT else 5
-            if end_index - index < size:
-                break
-            code = read_code(corrected_bits, index, size)
-            index += size
-            c = get_character(shift_table, code)
-            if c.startswith("CTRL_"):
-                latch_table = shift_table
-                shift_table = get_table(c[5])
-                if c[6] == "L":
-                    latch_table = shift_table
-            else:
-                output.append(ord(c[0]))
-                shift_table = latch_table
-
-    return output
-
-def read_code(bits, offset, length):
-    res = 0
-    for i in range(length):
-        res <<= 1
-        if bits[offset + i]:
-            res |= 1
-    return res
-
-def get_table(code):
-    if code == "L":
-        return Table.LOWER
-    elif code == "P":
-        return Table.PUNCT
-    elif code == "M":
-        return Table.MIXED
-    elif code == "D":
-        return Table.DIGIT
-    elif code == "B":
-        return Table.BINARY
+    if result["resultsCount"] > 0:
+        return bytes(result["binaryData"])
     else:
-        return Table.UPPER
-
-def get_character(table, code):
-    if table == Table.UPPER:
-        return UPPER_TABLE[code]
-    elif table == Table.LOWER:
-        return LOWER_TABLE[code]
-    elif table == Table.MIXED:
-        return MIXED_TABLE[code]
-    elif table == Table.DIGIT:
-        return DIGIT_TABLE[code]
-    elif table == Table.PUNCT:
-        return PUNCT_TABLE[code]
+        raise AztecError("No barcodes found")
